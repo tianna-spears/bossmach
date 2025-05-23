4ae47d7dd033e23e43826301c63a09188e999d4c
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-05-24 00:05:38 +0100

    router and controller added for meetings

--- project-bossm/server/meetingsRouter.js
+++ project-bossm/server/meetingsRouter.js
@@ -1,12 +1,31 @@
 const express = require('express')
-const meetingsRouter = express.Router;
+const meetingsRouter = express.Router()
+const {
+  getAllFromDatabase,
+  addToDatabase,
+  deleteAllFromDatabase
+} = require('./db')
 
-const meetings = [
-  { id: '1', date: 'May 21' },
-  { id: '2', date: 'May 28' },
-  { id: '3', date: 'May 31' }
-];
 
-meetingsRouter.get('/', (req,res) => {
-    res.send(meetings)
+meetingsRouter.get('/', (req, res, next) => {
+    const getMeetings = getAllFromDatabase('meetings')
+    res.send(getMeetings)
 })
+
+meetingsRouter.post('/', (req, res, next) => {
+  const newMeeting = addToDatabase('meetings', req.body)
+  if(!newMeeting){
+    return res.status(400).send('New meeting not created.')
+  }
+    res.status(201).send(newMeeting)
+})
+
+meetingsRouter.delete('/', (req, res, next) => {
+  const deleteMeetings = deleteAllFromDatabase('meetings')
+  if(!deleteMeetings) {
+    res.status(404).send('Meetings not deleted.')
+  }
+    res.status(204).send();
+})
+
+module.exports = meetingsRouter;
